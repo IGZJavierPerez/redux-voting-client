49d16343c29dabb0b8540f1062083e0148354d13
Javier Perez <surtich@gmail.com>
2015-09-19 14:00:59 +0200

    Structured css with SCSS

--- src/index.jsx
+++ src/index.jsx
@@ -12,7 +12,7 @@ import UhOh from './components/404';
 import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
 
-require('./style.css');
+require('./scss/main.scss');
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 socket.on('state', state =>
