7b16a6515411f7e8e98079ea9411d935277eacc2
IGZJavierPerez <surtich@gmail.com>
2015-09-21 13:11:01 +0200

    don't import react/addons when it is not needed

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react/addons';
+import React from 'react';
 import Router, {Route, Redirect} from 'react-router';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
